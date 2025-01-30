40a8d9c14478c569a058589e987459db90a2ded9
volkantutunculer <volkan.tutunculer@gmail.com>
2025-01-30 13:44:37 -0500

    update

--- 01-Basics/02_escape_sequences..js
+++ 01-Basics/02_escape_sequences..js
@@ -12,3 +12,23 @@ comments
  * JS Doc
  * JS Documentation
  */
+
+
+// Escape Squences
+// I like 'Aplle' and "Banana"
+
+console.log('I like \'Apple\' and "Banana"');
+
+console.log("I like 'Aplle' and \"Banana\"");
+
+console.log(`I like 'Apple' and "Banana"`); // ES6 feature
+
+console.log('\tHello, today is good day.\nI enjoy it so far!'); // \t - tab space, \n - new line.
+
+console.log(`   Hello, today is good day.
+I enjoy it so far!`); // With backticks you dont need to use \n for new line or \t for tab space.
+
+// My file can be found at C:\My Documents\MyFile.txt
+console.log('My file can be found at C:\\My Documents\\MyFile.txt'); // you need to use backword slash twice.
+
+console.log('Hellow\\\\\\Word'); // print Hello\\\\World
