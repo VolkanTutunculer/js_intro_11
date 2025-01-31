1ea4b716fa4ed61e80b407779b629817a9bee22f
volkantutunculer <volkan.tutunculer@gmail.com>
2025-01-31 13:13:10 -0500

    update

--- 03-Data_Types/03_objects_intro.js
+++ 03-Data_Types/03_objects_intro.js
@@ -44,3 +44,33 @@ program.start = 'Jan 27, 2025';
 program['duraction'] = '7 Months';
 
 console.log(program);
+
+
+const student = {
+
+    fullname: 'John Doe',
+    courses: ['Maath', 'Science'],
+    address: {
+        street: '123 Abc St',
+        number: null,
+        city: 'Chicago',
+        state: 'IL',
+        ZIPCode: '12345 1234'
+    }
+}
+
+// Adding a property to an existing object
+
+student.address.ZIPCode = 'M4P 1A5';
+student.age = 25;
+student['favDish'] = 'Pizza';
+
+console.log(student);
+
+console.log(student.courses[1]);
+
+// remove some properties from an existing object
+
+delete student.favDish;
+
+console.log(student); // { fullname: 'John Doe', courses: [ 'Maath', 'Science' ], address: { street: '123 Abc St', number: null, city: 'Chicago', state: 'IL', ZIPCode: 'M4P 1A5' }, age: 25 }
