d56a811c95d66a8be8b0790fda7133d4fe358e7f
volkantutunculer <volkan.tutunculer@gmail.com>
2025-01-30 23:10:53 -0500

    update

--- 02-variables/01_introduction.js
+++ 02-variables/01_introduction.js
@@ -1,3 +1,26 @@
-// let - const - var
+// let - const - var // we can use 3 of them
 
+// reservedkey variableName = value;
 let name = 'John';
+
+console.log(name);
+
+// Number data
+
+let age = 45;
+
+console.log(age);
+
+console.log(age + 5);
+
+// Declare a variable without initial value
+
+let city; // undefined  
+
+console.log(city);
+
+// assingning a value to a variable
+
+city = 'New York';
+
+console.log(city);
