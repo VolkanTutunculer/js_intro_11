6becbca25ca1a2cee5d895913d4521245aa01c47
volkantutunculer <volkan.tutunculer@gmail.com>
2025-01-31 11:17:46 -0500

    update

--- 03-Data_Types/02_arrays_intro.js
+++ 03-Data_Types/02_arrays_intro.js
@@ -7,4 +7,21 @@ const names = ['John', 'Alex', 'James', 'Max']; // String array
 const numbers = [5, 7, 10, 15]; // Number array
 
 console.log(names);
-console.log(numbers);
+console.log(numbers); 
+
+const favoriteMovies = ['Matrix', 'Avengers', 'Fast and Furious'];
+
+console.log(favoriteMovies);
+console.log(typeof favoriteMovies); // object
+
+// Acces an specific element in the array
+
+console.log(favoriteMovies[1]);
+
+let secondMovie = favoriteMovies[1];
+
+console.log(secondMovie);
+console.log(favoriteMovies [2]); // Fast and Furious
+console.log(typeof favoriteMovies[2]); // string
+
+console.log(favoriteMovies[3]); // undefined
