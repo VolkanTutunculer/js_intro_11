f1e4d583b9e66bc80bfc7f41cadc9ddad0fc6916
volkantutunculer <volkan.tutunculer@gmail.com>
2025-01-31 10:52:07 -0500

    update

---
+++ 03-Data_Types/02_arrays_intro.js
@@ -0,0 +1,10 @@
+// Arrays
+
+let studentName = 'Ayah';
+
+// Multiple names. numbers, objects. strings, prices
+const names = ['John', 'Alex', 'James', 'Max']; // String array
+const numbers = [5, 7, 10, 15]; // Number array
+
+console.log(names);
+console.log(numbers);
