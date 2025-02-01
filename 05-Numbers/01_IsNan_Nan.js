8871d08e6346014bb80fbe75f6d8a5b072de59b9
volkantutunculer <volkan.tutunculer@gmail.com>
2025-02-01 03:43:48 -0500

    update

---
+++ 05-Numbers/01_IsNan_Nan.js
@@ -0,0 +1,18 @@
+/*
+OBJECT: all built-in objects in programming languages have their own properties and functions.
+
+properties: information about the object what it has
+
+functions: abilities of the object what it does
+
+Number.MAX_SAFE_INTEGER; // prop
+
+Number.MIN_SAFE_INTEGER; // prop
+
+Math.PI; // 3.14
+
+Math.max(1, 2); // 2
+
+parantez varsa fonksiyon
+
+*/
