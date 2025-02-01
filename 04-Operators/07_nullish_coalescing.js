a146742b7fb83b673f310f81a75fc5bf11324a27
volkantutunculer <volkan.tutunculer@gmail.com>
2025-02-01 03:43:02 -0500

    update

--- 04-Operators/07_nullish_coalescing.js
+++ 04-Operators/07_nullish_coalescing.js
@@ -15,3 +15,25 @@ let nameInServer = null;
 
 nameInServer = nameInServer ?? 'customer';
 console.log(`Dear ${nameInServer}`);
+
+/*
+OBJECT: all built-in objects in programming languages have their own properties and functions.
+
+properties: information about the object what it has
+
+functions: abilities of the object what it does
+
+Number.MAX_SAFE_INTEGER; // prop
+
+Number.MIN_SAFE_INTEGER; // prop
+
+Math.PI; // 3.14
+
+Math.max(1, 2); // 2
+
+parantez varsa fonksiyon
+
+
+
+
+*/
