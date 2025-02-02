f7d2492d9d9e53badd2aa16068f2e9bc8bf97b02
volkantutunculer <volkan.tutunculer@gmail.com>
2025-02-02 12:43:40 -0500

    update

--- 07-Strings/09_practice.js
+++ 07-Strings/09_practice.js
@@ -17,4 +17,19 @@ let lastFive2 = str1.slice(str1.length - 5);
 console.log(lastFive);
 console.log(lastFive2);
 
+/*
+Assume that you are given a string of any length
 
+Get the first 3 characters and the last 3 characters
+Then, concatenate these in a new string
+
+JavaScript      -> Javipt
+Hello           -> Helllo
+123             -> 123123
+*/
+
+let str2 = 'Hello';
+
+let newString = `${str2.slice(0, 3)}${str2.slice(-3)}`;
+
+console.log(newString);
