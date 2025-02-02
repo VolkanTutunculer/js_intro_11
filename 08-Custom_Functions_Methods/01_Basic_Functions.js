b71fd3bd35ccc54ca86e02abe2587854288ad3e6
volkantutunculer <volkan.tutunculer@gmail.com>
2025-02-02 13:36:41 -0500

    update

---
+++ 08-Custom_Functions_Methods/01_Basic_Functions.js
@@ -0,0 +1,23 @@
+// Function Declaration
+
+function printHi() {
+    console.log('Hi');
+}
+
+printHi();
+
+/* my daily routine
+console.log('Perpare coffe');
+console.log('Take a shower');
+console.log('Drink coffe');
+console.log('Head to school');
+*/
+
+function runMyDaily() {
+console.log('Perpare coffe');
+console.log('Take a shower');
+console.log('Drink coffe');
+console.log('Head to school');
+}
+
+runMyDaily();
