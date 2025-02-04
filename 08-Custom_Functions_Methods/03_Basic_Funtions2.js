546774a4a39cad1922ad713e7ca409572cc42d60
volkan.tutunculer <you@example.com>
2025-02-03 19:49:28 -0500

    update

---
+++ 08-Custom_Functions_Methods/03_Basic_Funtions2.js
@@ -0,0 +1,41 @@
+/*
+Write a function which takes a number and return true if the number is even 
+or return false if the number is odd
+
+isEven(4)   -> true
+isEven(3)   -> false
+isEven(0)   -> true
+isEven(-2)  -> true
+isEven(-7)  -> false
+*/
+
+function isEven(number) {
+    return (number % 2) === 0;
+}
+console.log(isEven(4));
+console.log(isEven(3));
+console.log(isEven(0));
+console.log(isEven(-2));
+console.log(isEven(-7));
+
+
+
+/*
+Write a function named initials which takes 2 arguments as fname and lname and 
+return initials
+
+initials('John', 'Doe')      -> 'J.D.'
+initials('Adam', 'Smith')    -> 'A.S.'
+initials('Max', 'Smith')     -> 'M.S.'
+*/
+
+
+function initials(fname, lname) {
+    //return fname[0].toUpperCase() + '.' + lname[0].toUpperCase() + '.';
+    return `${fname[0]}.${lname[0]}.`;
+}
+
+
+console.log(initials('John', 'Doe'));
+console.log(initials('Adam', 'Smith'));
+console.log(initials('Max', 'Smith'));
