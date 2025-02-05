f868ce81a96ba60525e55111654059d0669c0195
volkan.tutunculer <you@example.com>
2025-02-04 21:43:49 -0500

    update

---
+++ homework/homework_01.js
@@ -0,0 +1,107 @@
+// TASK 1
+
+console.log('I start to practice "JavaScript" today, and I like it.');
+console.log('The secret of getting ahead is getting started.');
+console.log('"Don\'t limit yourself. "');
+console.log('Invest in your dreams. Grind now. Shine later.');
+console.log("It's not the load that breaks you down, it's the way you carry it.");
+console.log('The hard days are what make you stronger.');
+console.log('You can waste your lives drawing lines. Or you can live your life crossing them.');
+
+
+// TASK 2
+
+console.log('\tJavaScript is a high-level programming language primarily used to \nbuild web applications. It is not limited to the web; it can also be used \nfor the backend development with technologies like Node.js.\n\n\tIt is open source and has a huge community support, which means \nthere are plenty of resources and support available for learning. It \nuses a syntax like other programming languages and easy to learn'
+)
+
+// TASK 3
+
+let myAge = 34;
+let myFavoriteNumber = 13;
+let myHeight = 1.84;
+let myWeight = 104;
+let myFavoriteLetter = 'F';
+
+console.log('My Age:' + myAge);
+console.log('My Favorite Number:' + myFavoriteNumber);
+console.log('My Height:' + myHeight);
+console.log('My Height:' + myWeight);
+console.log('My Favorite Letter:' + myFavoriteLetter);
+
+// TASK 4
+
+let num1 = 25;
+let num2 = 35;
+
+console.log('The sum of the number = ' + (num1 + num2));
+
+// TASK 5
+
+let n1 = 5;
+let n2 = 7;
+
+console.log('The product of the number = ' + (n1 * n2));
+
+// TASK 6
+
+let number1 = 24;
+let number2 = 10;
+
+console.log('The sum of the number = ' + (number1 + number2));
+console.log('The product of the number = ' + (number1 * number2));
+console.log('The subtraction of the number = ' + (number1 - number2));
+console.log('The division of the number = ' + (number1 / number2));
+console.log('The remainder of the number = ' + (number1 % number2));
+
+// TASK 7
+
+let i1 = 7;
+let i2 = 11;
+
+console.log('The average of the numbers is: '+(i1 + i2)/2);
+
+// TASK 8
+
+let a1 = 6;
+let a2 = 10;
+let a3 = 12;
+let a4 = 15;
+let a5 = 17;
+
+console.log('The average of the numbers is: '+((a1 + a2 + a3 + a4 + a5)/5));
+
+// TASK 9
+
+let b1 = 5;
+let b2 = 6;
+let b3 = 10;
+
+console.log('The ' + b1 + ' multiplied with ' + b1 + ' is = ' + (b1 ** 2));
+console.log('The ' + b2 + ' multiplied with ' + b2 + ' is = ' + (b2 ** 2));
+console.log('The ' + b3 + ' multiplied with ' + b3 + ' is = ' + (b3 ** 2));
+
+// TASK 10
+
+let side = 7;
+
+console.log('The perimeter of the square = ' + (side * 4));
+console.log('The area of the square = ' + (side * side));
+
+// TASK 11
+
+let favBook = 'JS Algorithms & Data Structures';
+let favColor = 'Blue';
+let favNumber = 7;
+
+console.log('The favorite book is ' + favBook + '\nThe favorite color is ' + favColor + '\nThe favorite number is ' + favNumber);
+
+// TASK 12
+
+let firstName = 'John';
+let lastName = 'Doe';
+let age = 45;
+let emailAddress = 'johndoe@gmail.com';
+let phoneNumber = '(123) 123 1234';
+let address = '123 st Chicago IL 12345';
+
+console.log('\tUser who joined this program is ' + firstName + ' ' + lastName + '. ' + firstName + '`s age is ' + age + '.' + '\n' + firstName + '`s email address is ' + emailAddress + ', phone number is  ' + '\n' + phoneNumber + ', and address is ' + address + '.');
