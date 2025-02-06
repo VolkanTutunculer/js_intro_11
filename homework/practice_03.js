c795a2ead115458c9bbb0c6dcac24323b616a850
volkan.tutunculer <you@example.com>
2025-02-05 21:57:08 -0500

    update

--- homework/practice_03.js
+++ homework/practice_03.js
@@ -90,3 +90,137 @@ console.log(celciusToFahrenheit(20));
 console.log(celciusToFahrenheit(25));
 console.log(celciusToFahrenheit(0));
 console.log(celciusToFahrenheit(-10));
+
+// Or
+
+function celsiusToFahrenheit (celsius) {
+    return (celsius * 9) / 5 + 32;
+}
+
+console.log(celsiusToFahrenheit(20));
+console.log(celsiusToFahrenheit(25));
+console.log(celsiusToFahrenheit(0));
+console.log(celsiusToFahrenheit(-10));
+
+/*
+Write a function named as kgToPounds() which takes a number to be
+considered as Kilograms value and returns the Pounds value when
+invoked.
+Conversion Formula: The function uses the formula kilogram * 2.2 to
+perform the conversion from Kilograms to Pounds.
+•Multiply the Kilograms value by 2.2.
+Examples:
+kgToPounds(1)  -> 2.2
+kgToPounds(20)  -> 44
+kgToPounds(75)  -> 165
+kgToPounds(100)  -> 220
+*/
+
+function kgToPound (kilogram) {
+    return Math.round(kilogram * 2.2 * 10) / 10;
+}
+
+console.log(kgToPound(1));
+console.log(kgToPound(20));
+console.log(kgToPound(75));
+console.log(kgToPound(100));
+
+
+// Or ( Above is better because i made it :D )
+
+const kgToPounds = kg => {
+let pounds = (kg * 2.2).toFixed(1);
+
+let poundsAsStr = pounds + ""
+
+let poundsSplit = poundsAsStr.split('.');
+
+if (poundsSplit [1] == 0) pounds = poundsSplit [0]
+
+return pounds;
+}
+console.log(kgToPounds(1));
+console.log(kgToPounds(20));
+console.log(kgToPounds(75));
+console.log(kgToPounds(100));
+
+/*
+Write 2 functions named as rectangleArea() and rectanglePerimeter()
+which calculate the area and perimeter of a rectangle when invoked.
+NOTE: Assume the sides of the rectangle are x and y.
+Conversion Formula:
+•Area: x * y
+•Perimeter 2 * (x + y)
+Examples:
+rectangleArea(5, 4)  -> 20
+rectangleArea(3, 7)  -> 21
+rectangleArea(6, 10)  -> 60
+rectanglePerimeter(5, 4)  -> 18
+rectanglePerimeter(3, 7)  -> 20
+rectanglePerimeter(6, 10)  -> 32
+*/
+
+function rectangleArea (sd1, sd2) {
+    return sd1 * sd2;
+}
+function rectangleParimeter (sd1, sd2) {
+    return (sd1 + sd2) * 2;
+}
+console.log(rectangleArea(5, 4));
+console.log(rectangleArea(3, 7));
+console.log(rectangleArea(6, 10));
+console.log(rectangleParimeter(5, 4));
+console.log(rectangleParimeter(3, 7));
+console.log(rectangleParimeter(6, 10));
+
+/*
+Write 2 functions named as squareArea() and squarePerimeter() which
+calculate the area and perimeter of a square when invoked.
+NOTE: Assume the side of the square is x.
+Conversion Formula:
+•Area: x * x
+•Perimeter 4 * x
+Examples:
+squareArea(5)  -> 25
+squareArea(3)  -> 9
+squareArea(6)  -> 36
+squarePerimeter(5)  -> 20
+squarePerimeter(3)  -> 12
+squarePerimeter(6)  -> 24
+*/
+function quareArea(sd1) {
+    return sd1 * sd1;
+}
+function squarePerimeter (sd1) {
+    return (sd1 * 4);
+}
+console.log(quareArea(5));
+console.log(quareArea(3));
+console.log(quareArea(6));
+console.log(squarePerimeter(5));
+console.log(squarePerimeter(3));
+console.log(squarePerimeter(6));
+
+
+
+/*
+Write a function named as countWords() which takes a String and returns
+the number of words that are in the string
+countString("Hello, my name is John Doe") -> 6
+*/
+
+const countString = str => {
+    let strAsArr = str.split(/\s+/)
+    return strAsArr;
+}
+console.log(countString("Hello, my name is John Doe").length);
+
+// or Volkan's way
+function countstr (word) {
+    return word.split(/\s+/);
+}
+
+console.log(countstr('Hello,    my name     is John Doe').length);
+
+
+
