81084a74dc7d0895254102554e744bbe543f1a9e
volkan.tutunculer <you@example.com>
2025-02-08 12:15:45 -0500

    update

---
+++ 09-If-Else/02_if_else_statements.js
@@ -0,0 +1,12 @@
+console.log('I am learning if else statements');
+
+let mood = true;
+
+if (mood) {
+    console.log('ALL GOOD!')
+}
+else {
+    console.log('HELL')
+}
+
+console.log('End of the program');
