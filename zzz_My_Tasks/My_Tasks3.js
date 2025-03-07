a9245af20935f823110c341eec4438a08b0a0139
volkan.tutunculer <volkan.tutunculer@gmail.com>
2025-03-07 12:24:27 -0500

    update myTask3 and fix recapBilal

--- zzz_My_Tasks/My_Tasks3.js
+++ zzz_My_Tasks/My_Tasks3.js
@@ -15,3 +15,56 @@ function evenOrrOdd(number){
 }
 
 console.log(evenOrrOdd(randomNum));
+
+
+
+
+function factorial(n) {
+    if (n === 0 || n === 1) {
+        return 1;
+    }
+    else {
+        return n * factorial(n- 1);
+    }
+}
+
+console.log(factorial(3));
+
+
+function factor(n){
+    if (n === 0 || n=== 1){
+        return 1;
+
+    }
+    else {
+        return n * factor(n - 1);
+    }
+}
+console.log(factor(7));
+
+function reverseString(string) {
+    string = string.trim();
+
+    if (string.length < 2) {
+        return "";
+    }
+    let reversed = [];
+
+    for (let i = string.length - 1; i >= 0; i--) {
+        reversed.push(string[i]);
+    }
+    return reversed.join("")
+}
+
+console.log(reverseString("Tech Global"));
+
+
+function replaceFirstLastWord(str) {
+    str = str.trim();
+    if (str.length < 2) {
+        return "";
+    }
+    return str[str.length - 1] + str.slice(1, str.length - 1) + str[0]
+}
+
+console.log(replaceFirstLastWord('volkan'))
