f8aa76d5d5d64b16be570436655d376671dfc60a
volkan.tutunculer <volkan.tutunculer@gmail.com>
2025-03-25 22:00:51 -0400

    update

--- homework/practice09/test-actions.js
+++ homework/practice09/test-actions.js
@@ -1,4 +1,6 @@
-import { Actions } from "./Actions.js"; // if it was default we do not need {}.
+// import Actions module
+
+import { Actions } from "./Actions.js";
 
 /*
 Create a test method called as test1()
@@ -11,14 +13,64 @@ Create a test method called as test1()
     Title validated successfully
 */
 
+// Individual catch blocks for each async method executed
 function test1() {
-  Actions.visitUrl("www.google.com")
+  Actions.visitUrl("https://www.techglobalschool.com")
     .then((data) => {
       console.log(data);
+      Actions.login("TechGlobal", "Test1234")
+        .then((data) => {
+          console.log(data);
+          Actions.validateTitle("Batch11")
+            .then((data) => {
+              console.log(data);
+            })
+            .catch((err) => {
+              console.log(err);
+            });
+        })
+        .catch((err) => {
+          console.log(err);
+        });
     })
     .catch((err) => {
       console.log(err);
     });
 }
 
-test1();
+//test1();
+
+// One catch block to handle all the async methods and possible failures may come up
+
+function test2() {
+  Actions.visitUrl("www.google.com")
+    .then((data) => {
+      console.log(data);
+      return Actions.login("TechGlobal", "Test1234");
+    })
+    .then((data) => {
+      console.log(data);
+      return Actions.validateTitle("Batch11");
+    })
+    .then((data) => console.log(data))
+    .catch((err) => console.log(err));
+}
+
+//test2();
+
+// it wont show in browser because we need to store it
+async function test3() {
+  try {
+    const data1 = await Actions.visitUrl("www.google.com");
+    console.log(data1);
+    const data2 = await Actions.login("TechGlobal", "Test1234");
+    console.log(data2);
+    const data3 = await Actions.validateTitle("Batch11");
+    console.log(data3);
+  } catch (err) {
+    console.log(err);
+  }
+}
+
+test3();
+
