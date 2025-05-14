ca8c030631200548345a20368bf48d8e17964645
volkan.tutunculer <volkan.tutunculer@gmail.com>
2025-05-14 11:06:00 -0400

    first upload HW12

---
+++ homework/homework12.js
@@ -0,0 +1,16 @@
+// Task 1
+console.log("----Task 1----");
+
+function makeNegative(number) {
+  if (number >  0) return -1 * number;
+  else return number;
+}
+
+console.log(makeNegative(10));
+console.log(makeNegative(-7));
+console.log(makeNegative(0));
+console.log(makeNegative(0.45));
+
+
+// Task 2
+console.log("----Task 2----");
